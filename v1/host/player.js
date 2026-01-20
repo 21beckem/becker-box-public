1e7aa0a703ab69bbabb9d7337948db7629ed3ca2
Michael Becker <21beckem8@gmail.com>
2026-01-19 17:19:09 -0700

    removed disconnect timer

--- v1/host/player.js
+++ v1/host/player.js
@@ -16,16 +16,12 @@ export default class Player {
         this.remoteContainer.classList.add('connected');
         this.remoteContainer.querySelector('.disconnect').innerText = 'Disconnect';
         this.remoteContainer.querySelector('.disconnect').onclick = () => this.parent.removePlayer(this.slot);
-
-        this.#restartDisconnectTimer();
     }
     #initConn() {
         this.conn.on('open', () => {
-            this.#restartDisconnectTimer();
             this.conn.send({slot: this.slot});
         })
         this.conn.on('data', (data) => {
-            this.#restartDisconnectTimer();
             if (data.menuAction) {
                 switch (data.menuAction) {
                     case 'getDiscs':
@@ -66,20 +62,10 @@ export default class Player {
 
 
     
-    #removeDisconnectTimer() {
-        if (this.#disconnectTimeout)
-            clearTimeout(this.#disconnectTimeout);
-        this.#disconnectTimeout = null;
-    }
-    #restartDisconnectTimer() {
-        this.#removeDisconnectTimer();
-        this.#disconnectTimeout = setTimeout(() => this.#disconnect(), DISCONNECT_TIMEOUT_MS);
-    }
     #disconnect() {
         if (this.removed) return;
         this.removed = true;
 
-        this.#removeDisconnectTimer();
         console.warn(`Disconnecting slot ${this.slot} due to inactivity`);
         this.conn.close();
         this.pointer.remove();
